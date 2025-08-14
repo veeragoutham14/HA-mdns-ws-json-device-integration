a2b844f87d7efa3b3d456fdedf96ff5512b00aac
veeragoutham14 <153171280+veeragoutham14@users.noreply.github.com>
2025-08-14 13:02:58 +0200

    Update config_flow.py

--- custom_components/KaVo_Integration/config_flow.py
+++ custom_components/KaVo_Integration/config_flow.py
@@ -34,7 +34,7 @@ class TestChairConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         
         
 
-        await self.async_set_unique_id(name)
+        await self.async_set_unique_id(hostname)
         self._abort_if_unique_id_configured(updates={CONF_HOST: host})
 
         self.context.update({
